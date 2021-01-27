86faf433e5470e71608709d1f21c258d703688c3
tanay123456789 <tanaydwivedi456@gmail.com>
2021-01-28 01:30:49 +0530

    challenge accepted

---
+++ my-app/src/index.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  
+  <div>
+  <h1>Tanay's Netflix Pick</h1>,
+  <p>List of 5 Best Series.</p>
+  <ol>
+    <li>Dark</li>
+    <li>Extra curricualar</li>
+    <li>Death Note</li>
+    <li>Naruto</li>
+    <li>Scam 1992</li>
+  </ol>
+
+
+  </div>
+
+
+  
+  ,document.getElementById('root')
+);
+
+
